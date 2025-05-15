43aeba289de9daa3e53882557e7d27a7b5ca9b58
Jake Arkin <jarkin@mit.edu>
2025-05-15 14:12:17 -0400

    Add domain_type combobox

--- nlu_interface_rviz/include/nlu_interface_rviz/instruction_panel.hpp
+++ nlu_interface_rviz/include/nlu_interface_rviz/instruction_panel.hpp
@@ -7,43 +7,46 @@
 #include <std_msgs/msg/string.hpp>
 #include <ros_system_monitor_msgs/msg/node_info_msg.hpp>
 #include <omniplanner_msgs/msg/language_goal_msg.hpp>
+#include <omniplanner_msgs/msg/goto_points_goal_msg.hpp>
 
 // Qt
 #include <QLabel>
 #include <QTextEdit>
 #include <QLineEdit>
+#include <QComboBox>
 #include <QTimer>
 
 namespace nlu_interface_rviz {
 class InstructionPanel : public rviz_common::Panel
 {
-  Q_OBJECT
+    Q_OBJECT
 public:
-  explicit InstructionPanel(QWidget * parent = 0);
-  ~InstructionPanel() override = default;
-  void onInitialize() override;
+    explicit InstructionPanel(QWidget * parent = 0);
+    ~InstructionPanel() override = default;
+    void onInitialize() override;
 
 protected:
 
-  //void topicCallback(const std_msgs::msg::String & msg);
-  void handleLLMResponse( std_msgs::msg::String const & msg );
+    //void topicCallback(const std_msgs::msg::String & msg);
+    void handleLLMResponse( std_msgs::msg::String const & msg );
 
 
-  // ROS2 member variables
-  std::shared_ptr<rviz_common::ros_integration::RosNodeAbstractionIface> p_node_abstraction_;
-  rclcpp::Publisher<omniplanner_msgs::msg::LanguageGoalMsg>::SharedPtr instruction_publisher_;
-  rclcpp::Publisher<ros_system_monitor_msgs::msg::NodeInfoMsg>::SharedPtr system_monitor_publisher_;
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr llm_response_subscription_;
+    // ROS2 member variables
+    std::shared_ptr<rviz_common::ros_integration::RosNodeAbstractionIface> p_node_abstraction_;
+    rclcpp::Publisher<omniplanner_msgs::msg::LanguageGoalMsg>::SharedPtr instruction_publisher_;
+    rclcpp::Publisher<ros_system_monitor_msgs::msg::NodeInfoMsg>::SharedPtr system_monitor_publisher_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr llm_response_subscription_;
 
-  // GUI member variables
-  QTextEdit * p_llm_response_textbox_;
-  QLineEdit * p_prev_instruction_editor_;
-  QLineEdit * p_instruction_editor_;
-  QTimer * p_timer_;
+    // GUI member variables
+    QTextEdit * p_llm_response_textbox_;
+    QLineEdit * p_prev_instruction_editor_;
+    QLineEdit * p_instruction_editor_;
+    QComboBox * p_domain_type_combo_box_;
+    QTimer * p_timer_;
 
 private Q_SLOTS:
-  void publishInstruction( void );
-  void publishSystemMonitor( void );
+    void publishInstruction( void );
+    void publishSystemMonitor( void );
 }; // class InstructionPanel
 }  // namespace nlu_interface_rviz
 
--- nlu_interface_rviz/src/instruction_panel.cpp
+++ nlu_interface_rviz/src/instruction_panel.cpp
@@ -4,83 +4,92 @@
 
 namespace nlu_interface_rviz {
 InstructionPanel::InstructionPanel(QWidget* parent) : Panel(parent) {
-  // Create the layout for the instruction field
-  QHBoxLayout * p_instruction_layout = new QHBoxLayout;
-  p_instruction_layout->addWidget( new QLabel("Instruction:"));
-  p_instruction_editor_ = new QLineEdit;
-  p_instruction_layout->addWidget( p_instruction_editor_ );
+    // Create the layout for the domain type combo box
+    QHBoxLayout * p_domain_type_combo_box_layout = new QHBoxLayout;
+    p_domain_type_combo_box_layout->addWidget( new QLabel("Domain Type"));
+    p_domain_type_combo_box_ = new QComboBox;
+    p_domain_type_combo_box_->addItems({"default", "Pddl", "goto_points"});
+    p_domain_type_combo_box_layout->addWidget( p_domain_type_combo_box_ );
 
-  // Create the layout for a display of the previously published instruction
-  QHBoxLayout * p_prev_instruction_layout = new QHBoxLayout;
-  p_prev_instruction_layout->addWidget( new QLabel("Published Instruction:"));
-  p_prev_instruction_editor_ = new QLineEdit;
-  p_prev_instruction_editor_->setReadOnly(true);
-  p_prev_instruction_layout->addWidget( p_prev_instruction_editor_ );
+    // Create the layout for the instruction field
+    QHBoxLayout * p_instruction_layout = new QHBoxLayout;
+    p_instruction_layout->addWidget( new QLabel("Instruction:"));
+    p_instruction_editor_ = new QLineEdit;
+    p_instruction_layout->addWidget( p_instruction_editor_ );
 
-  // Create the layout for a display of the response from the LLM
-  QHBoxLayout * p_llm_response_layout = new QHBoxLayout;
-  p_llm_response_layout->addWidget( new QLabel("LLM Response"));
-  p_llm_response_textbox_ = new QTextEdit;
-  p_llm_response_textbox_->setReadOnly(true);
-  p_llm_response_layout->addWidget( p_llm_response_textbox_ );
+    // Create the layout for a display of the previously published instruction
+    QHBoxLayout * p_prev_instruction_layout = new QHBoxLayout;
+    p_prev_instruction_layout->addWidget( new QLabel("Published Instruction:"));
+    p_prev_instruction_editor_ = new QLineEdit;
+    p_prev_instruction_editor_->setReadOnly(true);
+    p_prev_instruction_layout->addWidget( p_prev_instruction_editor_ );
 
-  // Organize the layouts vertically
-  QVBoxLayout * p_layout = new QVBoxLayout;
-  p_layout->addLayout( p_instruction_layout );
-  p_layout->addLayout( p_prev_instruction_layout );
-  p_layout->addLayout( p_llm_response_layout );
-  setLayout( p_layout );
+    // Create the layout for a display of the response from the LLM
+    QHBoxLayout * p_llm_response_layout = new QHBoxLayout;
+    p_llm_response_layout->addWidget( new QLabel("LLM Response"));
+    p_llm_response_textbox_ = new QTextEdit;
+    p_llm_response_textbox_->setReadOnly(true);
+    p_llm_response_layout->addWidget( p_llm_response_textbox_ );
 
-  // Create a timer to regularly publish to the system monitor
-  QTimer * p_timer_ = new QTimer(this);
+    // Organize the layouts vertically
+    QVBoxLayout * p_layout = new QVBoxLayout;
+    p_layout->addLayout( p_domain_type_combo_box_layout );
+    p_layout->addLayout( p_instruction_layout );
+    p_layout->addLayout( p_prev_instruction_layout );
+    p_layout->addLayout( p_llm_response_layout );
+    setLayout( p_layout );
 
-  // Create Qt connections
-  QObject::connect( p_instruction_editor_, SIGNAL(returnPressed()), this, SLOT(publishInstruction()) );
-  QObject::connect( p_timer_, &QTimer::timeout, this, &InstructionPanel::publishSystemMonitor );
-  p_timer_->start(500);
-  return;
+    // Create a timer to regularly publish to the system monitor
+    QTimer * p_timer_ = new QTimer(this);
+
+    // Create Qt connections
+    QObject::connect( p_instruction_editor_, SIGNAL(returnPressed()), this, SLOT(publishInstruction()) );
+    QObject::connect( p_timer_, &QTimer::timeout, this, &InstructionPanel::publishSystemMonitor );
+    p_timer_->start(500);
+    return;
 }
 
 void InstructionPanel::onInitialize(){
-  p_node_abstraction_ = getDisplayContext()->getRosNodeAbstraction().lock();
-  rclcpp::Node::SharedPtr node = p_node_abstraction_->get_raw_node();
+    p_node_abstraction_ = getDisplayContext()->getRosNodeAbstraction().lock();
+    rclcpp::Node::SharedPtr node = p_node_abstraction_->get_raw_node();
 
-  // Create the publishers & subscriptions
-  instruction_publisher_ = node->create_publisher<omniplanner_msgs::msg::LanguageGoalMsg>("/omniplanner_node/language_planner/language_goal", 10);
-  system_monitor_publisher_ = node->create_publisher<ros_system_monitor_msgs::msg::NodeInfoMsg>("~/node_status", 1);
-  llm_response_subscription_ = node->create_subscription<std_msgs::msg::String>("~/llm_response", 10, std::bind(&InstructionPanel::handleLLMResponse, this, std::placeholders::_1));
-  return;
+    // Create the publishers & subscriptions
+    instruction_publisher_ = node->create_publisher<omniplanner_msgs::msg::LanguageGoalMsg>("/omniplanner_node/language_planner/language_goal", 10);
+    system_monitor_publisher_ = node->create_publisher<ros_system_monitor_msgs::msg::NodeInfoMsg>("~/node_status", 1);
+    llm_response_subscription_ = node->create_subscription<std_msgs::msg::String>("~/llm_response", 10, std::bind(&InstructionPanel::handleLLMResponse, this, std::placeholders::_1));
+    return;
 }
 
 void InstructionPanel::handleLLMResponse(std_msgs::msg::String const & msg ){
-  p_llm_response_textbox_->setText( msg.data.c_str() ); 
-  return;
+    p_llm_response_textbox_->setText( msg.data.c_str() ); 
+    return;
 }
 
 void InstructionPanel::publishInstruction( void ){
-  // Construct and publish the instruction text as a String message 
-  auto msg = omniplanner_msgs::msg::LanguageGoalMsg();
-  msg.robot_id = "NO ONE";
-  msg.command = p_instruction_editor_->text().toStdString();
-  instruction_publisher_->publish( msg );
+    // Construct and publish the instruction text as a String message 
+    auto msg = omniplanner_msgs::msg::LanguageGoalMsg();
+    msg.robot_id = "NO ONE";
+    msg.command = p_instruction_editor_->text().toStdString();
+    msg.domain_type = p_domain_type_combo_box_->currentText().toStdString();
+    instruction_publisher_->publish( msg );
 
-  // Update the display for the previous instruction
-  p_prev_instruction_editor_->setText( p_instruction_editor_->text() );
-  return;
+    // Update the display for the previous instruction
+    p_prev_instruction_editor_->setText( p_instruction_editor_->text() );
+    return;
 }
 
 void InstructionPanel::publishSystemMonitor( void ){
-  // Get the ROS2 node
-  auto p_node = p_node_abstraction_->get_raw_node();
+    // Get the ROS2 node
+    auto p_node = p_node_abstraction_->get_raw_node();
 
-  // Construct the message
-  auto msg = ros_system_monitor_msgs::msg::NodeInfoMsg();
-  msg.nickname = "nlu_rviz_panel";
-  msg.node_name = p_node->get_fully_qualified_name();
-  msg.status = ros_system_monitor_msgs::msg::NodeInfoMsg::NOMINAL;
-  msg.notes = "You gonna give me orders?";
-  system_monitor_publisher_->publish( msg );
-  return;
+    // Construct the message
+    auto msg = ros_system_monitor_msgs::msg::NodeInfoMsg();
+    msg.nickname = "nlu_rviz_panel";
+    msg.node_name = p_node->get_fully_qualified_name();
+    msg.status = ros_system_monitor_msgs::msg::NodeInfoMsg::NOMINAL;
+    msg.notes = "You gonna give me orders?";
+    system_monitor_publisher_->publish( msg );
+    return;
 }
 
 }  // namespace nlu_interface_rviz
