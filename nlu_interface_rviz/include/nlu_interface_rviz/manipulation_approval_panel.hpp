1b226de43222612e04dd18507cad1caa2f2a3944
Jake Arkin <jarkin@mit.edu>
2025-08-24 15:20:05 -0400

    Create initial separate rviz panels (WIP: panels are identical but both exist)

---
+++ nlu_interface_rviz/include/nlu_interface_rviz/manipulation_approval_panel.hpp
@@ -0,0 +1,82 @@
+#ifndef NLU_INTERFACE_RVIZ__MANIPULATION_APPROVAL_PANEL_HPP_
+#define NLU_INTERFACE_RVIZ__MANIPULATION_APPROVAL_PANEL_HPP_
+
+// ROS
+#include "rclcpp/rclcpp.hpp"
+#include <omniplanner_msgs/msg/goto_points_goal_msg.hpp>
+#include <omniplanner_msgs/msg/language_goal_msg.hpp>
+#include <ros_system_monitor_msgs/msg/node_info_msg.hpp>
+#include <rviz_common/panel.hpp>
+#include <rviz_common/ros_integration/ros_node_abstraction_iface.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <std_msgs/msg/bool.hpp>
+#include <std_msgs/msg/string.hpp>
+
+// Qt
+#include <QComboBox>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QRadioButton>
+#include <QSet>
+#include <QString>
+#include <QTextEdit>
+#include <QTimer>
+
+namespace nlu_interface_rviz {
+class ManipulationApprovalPanel : public rviz_common::Panel {
+  Q_OBJECT
+public:
+  explicit ManipulationApprovalPanel(QWidget *parent = 0);
+  ~ManipulationApprovalPanel() override = default;
+  void onInitialize() override;
+
+protected:
+  void publishManipulationResponse(bool const approve);
+
+  // ROS callbacks
+  void handleLLMResponse(std_msgs::msg::String::ConstSharedPtr msg);
+  void handleManipulationRequest(sensor_msgs::msg::Image::ConstSharedPtr msg,
+                                 std::string const &robot_id);
+  // Data members
+  QSet<QString> robot_ids_;
+
+  // ROS2 member variables
+  std::shared_ptr<rviz_common::ros_integration::RosNodeAbstractionIface>
+      p_node_abstraction_;
+  rclcpp::Publisher<omniplanner_msgs::msg::LanguageGoalMsg>::SharedPtr
+      instruction_publisher_;
+  rclcpp::Publisher<ros_system_monitor_msgs::msg::NodeInfoMsg>::SharedPtr
+      system_monitor_publisher_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr
+      llm_response_subscription_;
+  // map of robot ids to manipulation request subscriptions and approval
+  // publishers
+  std::map<std::string,
+           rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr>
+      manipulation_request_subscriptions_;
+  std::map<std::string, rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr>
+      manipulation_approval_publishers_;
+
+  // GUI member variables
+  QTextEdit *p_llm_response_textbox_;
+  QLineEdit *p_prev_instruction_editor_;
+  QLineEdit *p_instruction_editor_;
+  QComboBox *p_domain_type_combo_box_;
+  QComboBox *p_robot_id_combo_box_;
+  QComboBox *p_manipulation_robot_id_combo_box_;
+  QLabel *p_manipulation_image_label_; // TODO: Display a sensor_msgs/Image as a
+                                       // QLabel
+  QPushButton *p_manipulation_approve_push_button_;
+  QPushButton *p_manipulation_reject_push_button_;
+  QTimer *p_timer_;
+
+private Q_SLOTS:
+  void publishInstruction(void);
+  void publishManipulationApproval(void);
+  void publishManipulationRejection(void);
+  void publishSystemMonitor(void);
+}; // class ManipulationApprovalPanel
+} // namespace nlu_interface_rviz
+
+#endif // NLU_INTERFACE_RVIZ__MANIPULATION_APPROVAL_PANEL_HPP_
---
+++ nlu_interface_rviz/src/manipulation_approval_panel.cpp
@@ -0,0 +1,232 @@
+#include <QImage>
+#include <QVBoxLayout>
+#include <cv_bridge/cv_bridge.hpp>
+#include <nlu_interface_rviz/manipulation_approval_panel.hpp>
+#include <rviz_common/display_context.hpp>
+
+namespace nlu_interface_rviz {
+ManipulationApprovalPanel::ManipulationApprovalPanel(QWidget *parent)
+    : Panel(parent) {
+  // Create the layout for the domain type combo box
+  QHBoxLayout *p_domain_type_combo_box_layout = new QHBoxLayout;
+  p_domain_type_combo_box_layout->addWidget(new QLabel("Domain Type"));
+  p_domain_type_combo_box_ = new QComboBox;
+  p_domain_type_combo_box_->addItems({"default", "Pddl", "goto_points"});
+  p_domain_type_combo_box_layout->addWidget(p_domain_type_combo_box_);
+
+  // Create the layout for the robot id combo box
+  QHBoxLayout *p_robot_id_combo_box_layout = new QHBoxLayout;
+  p_robot_id_combo_box_layout->addWidget(new QLabel("Robot ID"));
+  p_robot_id_combo_box_ = new QComboBox;
+  p_robot_id_combo_box_layout->addWidget(p_robot_id_combo_box_);
+
+  // Create the layout for the instruction field
+  QHBoxLayout *p_instruction_layout = new QHBoxLayout;
+  p_instruction_layout->addWidget(new QLabel("Instruction:"));
+  p_instruction_editor_ = new QLineEdit;
+  p_instruction_layout->addWidget(p_instruction_editor_);
+
+  // Create the layout for a display of the previously published instruction
+  QHBoxLayout *p_prev_instruction_layout = new QHBoxLayout;
+  p_prev_instruction_layout->addWidget(new QLabel("Published Instruction:"));
+  p_prev_instruction_editor_ = new QLineEdit;
+  p_prev_instruction_editor_->setReadOnly(true);
+  p_prev_instruction_layout->addWidget(p_prev_instruction_editor_);
+
+  // Create the layout for a display of the response from the LLM
+  QHBoxLayout *p_llm_response_layout = new QHBoxLayout;
+  p_llm_response_layout->addWidget(new QLabel("LLM Response"));
+  p_llm_response_textbox_ = new QTextEdit;
+  p_llm_response_textbox_->setReadOnly(true);
+  p_llm_response_layout->addWidget(p_llm_response_textbox_);
+
+  // Create the layout for a display of a manipulation request image
+  QVBoxLayout *p_manipulation_request_layout = new QVBoxLayout;
+  p_manipulation_request_layout->addWidget(
+      new QLabel("Manipulation Approval Request"));
+  p_manipulation_image_label_ = new QLabel("Manipulation Image Placeholder");
+  p_manipulation_request_layout->addWidget(p_manipulation_image_label_);
+
+  // Create the layout for the approval robot id
+  QHBoxLayout *p_manipulation_robot_id_combo_box_layout = new QHBoxLayout;
+  p_manipulation_robot_id_combo_box_layout->addWidget(new QLabel("Robot ID"));
+  p_manipulation_robot_id_combo_box_ = new QComboBox;
+  p_manipulation_robot_id_combo_box_layout->addWidget(
+      p_manipulation_robot_id_combo_box_);
+
+  QHBoxLayout *p_manipulation_approval_layout = new QHBoxLayout;
+  p_manipulation_approve_push_button_ = new QPushButton("Approve");
+  p_manipulation_approval_layout->addWidget(
+      p_manipulation_approve_push_button_);
+  p_manipulation_reject_push_button_ = new QPushButton("Reject");
+  p_manipulation_approval_layout->addWidget(p_manipulation_reject_push_button_);
+
+  // Organize the layouts vertically
+  QVBoxLayout *p_layout = new QVBoxLayout;
+  p_layout->addLayout(p_domain_type_combo_box_layout);
+  p_layout->addLayout(p_robot_id_combo_box_layout);
+  p_layout->addLayout(p_instruction_layout);
+  p_layout->addLayout(p_prev_instruction_layout);
+  p_layout->addLayout(p_llm_response_layout);
+  p_layout->addLayout(p_manipulation_request_layout);
+  p_layout->addLayout(p_manipulation_robot_id_combo_box_layout);
+  p_layout->addLayout(p_manipulation_approval_layout);
+  setLayout(p_layout);
+
+  // Create a timer to regularly publish to the system monitor
+  QTimer *p_timer_ = new QTimer(this);
+
+  // Create Qt connections
+  QObject::connect(p_instruction_editor_, SIGNAL(returnPressed()), this,
+                   SLOT(publishInstruction()));
+  QObject::connect(p_manipulation_approve_push_button_, SIGNAL(clicked()), this,
+                   SLOT(publishManipulationApproval()));
+  QObject::connect(p_manipulation_reject_push_button_, SIGNAL(clicked()), this,
+                   SLOT(publishManipulationRejection()));
+  QObject::connect(p_timer_, &QTimer::timeout, this,
+                   &ManipulationApprovalPanel::publishSystemMonitor);
+  p_timer_->start(500);
+  return;
+}
+
+void ManipulationApprovalPanel::onInitialize() {
+  p_node_abstraction_ = getDisplayContext()->getRosNodeAbstraction().lock();
+  rclcpp::Node::SharedPtr node = p_node_abstraction_->get_raw_node();
+
+  // Get the robot ids & populate the relevant combo boxes
+  auto param_robot_ids = node->declare_parameter<std::vector<std::string>>(
+      "robot_ids", {"euclid"});
+  for (auto &s : param_robot_ids) {
+    robot_ids_.insert(s.c_str());
+  }
+
+  p_robot_id_combo_box_->addItems(
+      QList<QString>(robot_ids_.begin(), robot_ids_.end()));
+
+  p_manipulation_robot_id_combo_box_->addItems(
+      QList<QString>(robot_ids_.begin(), robot_ids_.end()));
+
+  // Create the publishers
+  instruction_publisher_ =
+      node->create_publisher<omniplanner_msgs::msg::LanguageGoalMsg>(
+          "omniplanner_node/language_planner/language_goal", 10);
+  system_monitor_publisher_ =
+      node->create_publisher<ros_system_monitor_msgs::msg::NodeInfoMsg>(
+          "~/node_status", 1);
+  // Create a manipulation approval publisher and request subscriptions
+  // namespaced for each robot
+  for (auto const &q_robot_id : robot_ids_) {
+    auto const robot_id = q_robot_id.toStdString();
+    auto const request_topic =
+        "/" + robot_id + "/spot_executor_node/annotated_image";
+    manipulation_request_subscriptions_.emplace(
+        robot_id,
+        node->create_subscription<sensor_msgs::msg::Image>(
+            request_topic, 10,
+            [this, robot_id](sensor_msgs::msg::Image::ConstSharedPtr msg) {
+              this->handleManipulationRequest(msg, robot_id);
+            }));
+    auto const approval_topic =
+        "/" + robot_id + "/spot_executor_node/pick_confirmation";
+    manipulation_approval_publishers_.emplace(
+        robot_id,
+        node->create_publisher<std_msgs::msg::Bool>(approval_topic, 1));
+  }
+  // Create the subscriptions
+  llm_response_subscription_ = node->create_subscription<std_msgs::msg::String>(
+      "~/llm_response", 10, [this](std_msgs::msg::String::ConstSharedPtr msg) {
+        this->handleLLMResponse(msg);
+      });
+  return;
+}
+
+void ManipulationApprovalPanel::handleLLMResponse(
+    std_msgs::msg::String::ConstSharedPtr msg) {
+  p_llm_response_textbox_->setText(msg->data.c_str());
+  return;
+}
+
+void ManipulationApprovalPanel::handleManipulationRequest(
+    sensor_msgs::msg::Image::ConstSharedPtr msg, std::string const &robot_id) {
+
+  // Set the combo box to the robot id
+  p_manipulation_robot_id_combo_box_->setCurrentText(robot_id.c_str());
+
+  // Convert msg to cv::Mat
+  cv_bridge::CvImagePtr cv_ptr;
+  try {
+    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+  } catch (cv_bridge::Exception &e) {
+    throw std::runtime_error("cv_bridge exception: " + std::string(e.what()));
+  }
+  auto cv_mat = cv_ptr->image;
+
+  // Convert cv::Mat to QImage
+  auto qimage = QImage(cv_mat.data, cv_mat.cols, cv_mat.rows, cv_mat.step,
+                       QImage::Format_BGR888);
+
+  // Convert QImage to QPixmap
+  auto qpixmap = QPixmap::fromImage(qimage);
+  qpixmap = qpixmap.scaled(320, 240, Qt::KeepAspectRatio);
+
+  // Set the QLabel's pixmap
+  p_manipulation_image_label_->setPixmap(qpixmap);
+  return;
+}
+
+void ManipulationApprovalPanel::publishInstruction(void) {
+  // Construct and publish the instruction text as a String message
+  auto msg = omniplanner_msgs::msg::LanguageGoalMsg();
+  msg.robot_id = p_robot_id_combo_box_->currentText().toStdString();
+  msg.command = p_instruction_editor_->text().toStdString();
+  msg.domain_type = p_domain_type_combo_box_->currentText().toStdString();
+  instruction_publisher_->publish(msg);
+
+  // Update the display for the previous instruction
+  p_prev_instruction_editor_->setText(p_instruction_editor_->text());
+  return;
+}
+
+void ManipulationApprovalPanel::publishManipulationApproval(void) {
+  publishManipulationResponse(true);
+  return;
+}
+
+void ManipulationApprovalPanel::publishManipulationRejection(void) {
+  publishManipulationResponse(false);
+  return;
+}
+
+void ManipulationApprovalPanel::publishManipulationResponse(
+    bool const approve) {
+  // Construct the approval message (bool)
+  auto msg = std_msgs::msg::Bool();
+  msg.data = approve;
+  // Get the publisher for the selected robot id
+  auto it_publisher = manipulation_approval_publishers_.find(
+      p_manipulation_robot_id_combo_box_->currentText().toStdString());
+  assert(it_publisher != manipulation_approval_publishers_.end());
+  // Publish the approval message
+  it_publisher->second->publish(msg);
+  return;
+}
+
+void ManipulationApprovalPanel::publishSystemMonitor(void) {
+  // Get the ROS2 node
+  auto p_node = p_node_abstraction_->get_raw_node();
+
+  // Construct the message
+  auto msg = ros_system_monitor_msgs::msg::NodeInfoMsg();
+  msg.nickname = "nlu_rviz_panel";
+  msg.node_name = p_node->get_fully_qualified_name();
+  msg.status = ros_system_monitor_msgs::msg::NodeInfoMsg::NOMINAL;
+  msg.notes = "You gonna give me orders?";
+  system_monitor_publisher_->publish(msg);
+  return;
+}
+
+} // namespace nlu_interface_rviz
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(nlu_interface_rviz::ManipulationApprovalPanel,
+                       rviz_common::Panel)
